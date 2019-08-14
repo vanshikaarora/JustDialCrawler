9b3e0a6571af5c44db585d74dea5f6fd32b8032d
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-08-15 00:53:56 +0530

    // Stash

--- final_justdial_scrapper.py
+++ final_justdial_scrapper.py
@@ -1,93 +1,175 @@
-from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup
+import urllib
+from selenium import webdriver
 import sys
+import time
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 import csv
 
-link_list=[]
-sturl = input('Please enter url name')
-orig_stdout = sys.stdout
-f = open('out2.html', 'w')
-sys.stdout = f
-sys.stdout = orig_stdout
-f.close()
-url=sturl
-#print(sturl)
-file_name=str(sturl.split('/')[3]+'_'+sturl.split('/')[4]+".csv")
-#print(file_name)
-out_file=open(file_name,"w")
-writer=csv.writer(out_file)
-writer.writerow(["type","name","streetAddress","addressLocality","postalCode","addressRegion","addressCountry","telephone1","telephone2"])
-
-def generateLink():
-	i=1
-	req = Request(str(sturl).strip(), headers={'User-Agent': 'Mozilla/5.0'})
-	webpage = urlopen(req).read()
-	parser = BeautifulSoup(webpage,'html.parser')
-	soap=parser.find_all('li',{"class":"cntanr"})
-	while bool(parser.find('li',{"class":"cntanr"}))==True:
-		url=sturl+'/page-'+str(i)
-		i=i+1
-		print(url)
-		req = Request(str(url).strip(), headers={'User-Agent': 'Mozilla/5.0'})
-		webpage = urlopen(req).read()
-		parser = BeautifulSoup(webpage,'html.parser')
-		#print(parser)
-		soap=parser.find_all('li',{"class":"cntanr"})
-		#print(soap)
-		getLink(soap)
+# from selenium.webdriver.firefox.webdriver import WebDriver
+# from selenium import webdriver
+# browser = webdriver.Firefox()
+browser = webdriver.Chrome() 
 
 
-def getLink(soap):
-	for s in soap:
-		link_list.append(s["data-href"])
-		#print(s["data-href"])
+def innerHTML(element):
+    return element.decode_contents(formatter="html")
 
-def getData():
+def get_name(body):
+    return body.find('span', {'class':'jcn'}).a.string
 
-	print(link_list)
-	for link in link_list:
-		#print('1'+link)
-		req1 = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-		webpage1 = urlopen(req1).read()
-		parser = BeautifulSoup(webpage1,'html.parser')
-		file_object=open('data.txt','w')
-		file_object.write(str(parser))
-		file_object.close()
-		p=parser.find_all('script')
-		data=p[1].text
-		#print('2'+data)
-		type=data.split('@type": "')[1].split('"')[0]
-		#print(type)
-		name=data.split('name": "')[1].split('"')[0]
-		#print('3'+name)
-		streetAddress=data.split('streetAddress": "')[1].split('"')[0]
-		#print(streetAddress)
-		addressLocality=data.split('addressLocality": "')[1].split('"')[0]
-		#print(addressLocality)
-		postalCode=data.split('postalCode": "')[1].split('"')[0]
-		#print(postalCode)
-		addressRegion=data.split('addressRegion": "')[1].split('"')[0]
-		#print(addressRegion)
-		addressCountry=data.split('addressCountry": "')[1].split('"')[0]
-		#print(addressCountry)
-		if data.count('telephone')!=0:
-			telephone=data.split('telephone": ["')[1].split('"]')[0]
-			print(telephone)
-			c=telephone.count(',')
-			if c==0:
-				telephone1=telephone.split('"')[0]
-				telephone2=''
-			elif c>=1:
-				telephone1=telephone.split('"')[0]
-				telephone2=telephone.split('","')[1].split('"')[0]
-		else:
-			telephone1=''
-			telephone2=''
-		#print(telephone1)
-		#print(telephone2)
-		writer.writerow([type,name,streetAddress,addressLocality,postalCode,addressRegion,addressCountry,telephone1,telephone2])
-		#writer.close()
+def which_digit(html):
+    mappingDict={'icon-ji':9,
+                'icon-lk':8,
+                'icon-nm':7,
+                'icon-po':6,
+                'icon-rq':5,
+                'icon-ts':4,
+                'icon-vu':3,
+                'icon-wx':2,
+                'icon-yz':1,
+                'icon-acb':0,
+                }
+    return mappingDict.get(html,'')
 
-generateLink()
-#print(link_list)
-getData()
+def get_phone_number(body):
+    i=0
+    phoneNo = "No Number!"
+    try:
+            
+        for item in body.find('p',{'class':'contact-info'}):
+            i+=1
+            if(i==2):
+                phoneNo=''
+                try:
+                    for element in item.find_all(class_=True):
+                        classes = []
+                        classes.extend(element["class"])
+                        phoneNo+=str((which_digit(classes[1])))
+                except:
+                    pass
+    except:
+        pass
+    body = body['data-href']
+    browser.get(body)
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    for a in soup.find_all('a', {"id":"whatsapptriggeer"} ):
+        # print (a)
+        phoneNo = str(a['href'][-10:])
+
+
+    return phoneNo
+
+def get_rating(body):
+    rating = 0.0
+    text = body.find('span', {'class':'star_m'})
+    if text is not None:
+        for item in text:
+            rating += float(item['class'][0][1:])/10
+
+    return rating
+
+def get_rating_count(body):
+    text = body.find('span', {'class':'rt_count'}).string
+
+    # Get only digits
+    rating_count =''.join(i for i in text if i.isdigit())
+    return rating_count
+
+def get_address(body):
+    return body.find('span', {'class':'mrehover'}).text.strip()
+
+def get_location(body):
+    text = body.find('a', {'class':'rsmap'})
+    if text == None:
+        return
+    text_list = text['onclick'].split(",")
+
+    latitutde = text_list[3].strip().replace("'", "")
+    longitude = text_list[4].strip().replace("'", "")
+
+    return latitutde + ", " + longitude
+
+page_number = 1
+service_count = 1
+
+
+fields = ['Name', 'Phone', 'Rating', 'Rating Count', 'Address', 'Location']
+out_file = open('Main.csv','w')
+csvwriter = csv.DictWriter(out_file, delimiter=',', fieldnames=fields)
+
+# Write fields first
+csvwriter.writerow(dict((fn,fn) for fn in fields))
+check = 0
+while True:
+
+    # Check if reached end of result
+    if page_number > 50:
+        break
+
+    url="https://www.justdial.com/Jaipur/Car-Dealers/page-%s" % (page_number)
+    req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
+    page = urllib.request.urlopen( req )
+
+    browser.get(url)
+    if(check!=1):
+        print("LOGIN THROUGH OPEN BROWSER NOW!")
+        # browser.execute_script('window.alert("Login now, you have 30 seconds")')
+        time.sleep(31)
+        check = 1
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    for tag2 in soup.find_all(attrs={"class":"lng_cont_name"}):
+        print (tag2.text)
+    spans = soup.find_all("span", {"class": "adrstxt whatsapptxt lng_commn"})
+    for span in spans:
+        links = span.find_all('a')
+        for link in links:
+            print (link.text)
+            print (link.getText())
+
+    # page=urllib2.urlopen(url)
+
+    soup = BeautifulSoup(page.read(), "html.parser")
+    services = soup.find_all('li', {'class': 'cntanr'})
+
+
+    # Iterate through the 10 results in the page
+    for service_html in services:
+
+        # Parse HTML to fetch data
+        dict_service = {}
+        name = get_name(service_html)
+        phone = get_phone_number(service_html)
+        rating = get_rating(service_html)
+        count = get_rating_count(service_html)
+        address = get_address(service_html)
+        location = get_location(service_html)
+        if name != None:
+            dict_service['Name'] = name
+        if phone != None:
+            print ("getting phone number")
+            dict_service['Phone'] = phone
+        if rating != None:
+            dict_service['Rating'] = rating
+        if count != None:
+            dict_service['Rating Count'] = count
+        if address != None:
+            dict_service['Address'] = address
+        if location != None:
+            dict_service['Address'] = location
+
+        # Write row to CSV
+        csvwriter.writerow(dict_service)
+
+        str1 = "#" + str(service_count) + " " , dict_service
+        print(str1)
+        service_count += 1
+
+    page_number += 1
+
+out_file.close()
+browser.close()
+
+
